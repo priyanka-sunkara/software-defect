6bea7fc3d20c6a0268a36d0a29788156048fbb73
chandu <chandusubramanyam1705@gmail.com>
2024-06-18 07:43:00 -0700

    deploy

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(_name_)
+
+# Load the trained Naive Bayes model
+with open('naive_bayes_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(request.form['feature1']), float(request.form['feature2'])]
+    # Add more features as needed
+    prediction = model.predict([features])
+    return jsonify({'prediction': int(prediction[0])})
+
+if _name_ == '_main_':
+    app.run(debug=True)
