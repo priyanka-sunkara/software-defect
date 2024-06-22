43ec306731f7f6ae12dbc5172f6c4eebc26f0d0f
chandu <chandusubramanyam1705@gmail.com>
2024-06-22 10:57:12 -0700

    deploy

--- script.js
+++
@@ -1,5 +0,0 @@
-var t = document.querySelector('#mytemplate');
-// Populate the src attribute at runtime
-t.content.querySelector('img').src = 'logo.png';
-var clone = document.importNode(t.content, true);
-document.body.appendChild(clone);
